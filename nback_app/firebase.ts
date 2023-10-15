8707659e268da12db8794b1e1acc15c59564eb9f
shotarai <second.arasho@outlook.jp>
2023-10-15 17:08:02 +0900

    gitignoreにconfigファイルを加えた

--- nback_app/firebase.ts
+++ nback_app/firebase.ts
@@ -2,15 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import config from './config';
 
-const FirebaseApp = initializeApp({
-  apiKey: "AIzaSyA92pJc4ypDO9O0WFoyXumKi1b-BqY6h6Y",
-  authDomain: "nbackapp-fe81b.firebaseapp.com",
-  projectId: "nbackapp-fe81b",
-  storageBucket: "nbackapp-fe81b.appspot.com",
-  messagingSenderId: "1031840519506",
-  appId: "1:1031840519506:web:2755f6eee68c47776e568b",
-});
+const firebaseConfig = {
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId,
+};
+
+const FirebaseApp = initializeApp(firebaseConfig);
 
 export const storage = getStorage(FirebaseApp);
 export const auth = getAuth(FirebaseApp);
